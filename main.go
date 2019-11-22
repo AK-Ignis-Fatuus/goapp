4f81e3614a6f7a890e320094e90f0948ac5a51c6
Hy6axa <andrdsniper@gmail.com>
2019-11-22 17:27:20 +0200

    code update

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
   "fmt"
   "github.com/gorilla/mux"
   "net/http"
-  //"database/sql"
-  //_"github.com/go-sql-driver/mysql"
-  //"io/ioutil"
+  "encoding/json"
+  "math/rand"
+  "strconv"
 )
 
 type Task struct {
@@ -17,32 +17,73 @@ type Task struct {
 
 var tasks []Task
 
-//var db *sql.DB
+func getTasks(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(tasks)
+}
 
+func createTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  var task Task
+  _ = json.NewDecoder(r.Body).Decode(&task)
+  task.ID = strconv.Itoa(rand.Intn(1000000))
+  tasks = append(tasks, task)
+  json.NewEncoder(w).Encode(&task)
+}
 
+func getTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for _, item := range tasks {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Task{})
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range tasks {
+    if item.ID == params["id"] {
+      tasks = append(tasks[:index], tasks[index+1:]...)
+      var task Task
+      _ = json.NewDecoder(r.Body).Decode(&task)
+      task.ID = params["id"]
+      tasks = append(tasks, task)
+      json.NewEncoder(w).Encode(&task)
+      
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(tasks)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range tasks {
+    if item.ID == params["id"] {
+      tasks = append(tasks[:index], tasks[index+1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(tasks)
+}
 
 func main() {
   fmt.Println("It works!!!")
-  router := mux.NewRouter()
-  
-  configureApp()
-  
-  /*db, err := sql.Open("mysql", "{db_user}:{db_pass}@({db_host}:{db_port})/{db_db}?parseTime=true")
-  if err != nil {
-    log.Fatal(err)
-  }
-  if err := db.Ping(); err != nil {
-    log.Fatal(err)
-  }*/
+  router := mux.NewRouter()	
   
   tasks = append(tasks, Task{ID: "1", Description: "My first Task", Status: false})
   
-  router.HandleFunc("/api/v1/tasks", getTasks).Methods("GET")
-  router.HandleFunc("/api/v1/tasks", createTask).Methods("POST")
-  router.HandleFunc("/api/v1/tasks/{id}", getTask).Methods("GET")
-  router.HandleFunc("/api/v1/tasks/{id}", updateTask).Methods("PUT")
-  router.HandleFunc("/api/v1/tasks/{id}", deleteTask).Methods("DELETE")
+  router.HandleFunc("/tasks", getTasks).Methods("GET")
+  router.HandleFunc("/tasks", createTask).Methods("POST")
+  router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+  router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+  router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
   
-  
-  http.ListenAndServe(":"+ config["app_port"], router)
+  http.ListenAndServe(":9876", router)
 }
