d626cff027174620f0488c3e898f4b0db9ae7875
Andrii Kramskiy <49439783+Hy6axa@users.noreply.github.com>
2019-11-26 13:08:53 +0200

    Update main.go

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 )
 
 const (
-	DB_USER     = ""
-	DB_PASSWORD = ""
-	DB_NAME     = ""
-  DB_HOST     = ""
-  DB_PORT     = ""
+  DB_USER= 
+  DB_PASSWORD= 
+  DB_NAME= 
+  DB_HOST= 
+  DB_PORT= 
 )
 
 type Task struct {
